88d0a54e4cd025677ffbcc9fc86c669b5a45670f
Svetlana.Syo <svetlana.syo@gmail.com>
2018-05-29 12:54:08 +0200

    funkcja importu danych z csv zwraca liste slownikow"

--- persistence.py
+++ persistence.py
@@ -9,4 +9,22 @@ def import_data_from_file(file_name):
             list_of_dict.append(row)
         return list_of_dict
 
-print(import_data_from_file('sample_data/question.csv'))
+
+def export_data_to_file(file_name):
+    with open(file_name, 'r') as csv_file:
+        csv_reader=csv.DictReader(csv_file)
+        with open(file_name, 'w') as new_file:
+            fieldnames = create_list_of_headers(list)
+            csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter=',')
+            csv_writer.writeheader()
+            
+            for line in csv
+
+with open('new_test.csv', 'w') as new_file:
+        fieldnames = ['Stock','Price','Quantity']
+        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames,delimiter=',')
+
+        csv_writer.writeheader()
+
+        for line in csv_reader:
+            csv_writer.writerow(line)
