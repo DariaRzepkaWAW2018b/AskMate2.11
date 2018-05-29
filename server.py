7ea1debae3b6fd02b794f4aa1757e83a76618b81
bartek <bartosz.dziedzic94@gmail.com>
2018-05-29 14:04:02 +0200

    proba for sveta

--- server.py
+++ server.py
@@ -3,15 +3,18 @@ from flask import Flask, request, render_template
 app=Flask(__name__)
 
 @app.route("/")
-@app.route('/list')
+@app.route('/list', methods=["POST", "GET"])
 def index():
-    return render_template("base.html")
+    return render_template("list_of_question.html")
 
 @app.route("/addanswer")
 def addanswer():
    return render_template("addanswer.html")
 
-
+@app.route("/addquestion", methods=['POST'])
+def addquestion():
+    return render_template('addquestion.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,
+            port = 5000)
