06cd342a9455ec41c832eebb743539f232971117
Daria Rzepka <dariarz0110@gmail.com>
2018-05-30 14:30:36 +0200

    creating new id for questions and answers

--- logic.py
+++ logic.py
@@ -1,4 +1,4 @@
-import persistence.py
+import persistence
 
 
 def delete_by_id(qa, id):
@@ -20,8 +20,8 @@ def delete_by_id(qa, id):
             if str(entry['question_id']) == str(id):
                 id_index_number = counter
         del a_data[id_index_number]
-            persistence.export_data_to_file("sample_data/question.csv", q_data)
-            persistence.export_data_to_file("sample_data/answer.csv", a_data)
+        persistence.export_data_to_file("sample_data/question.csv", q_data)
+        persistence.export_data_to_file("sample_data/answer.csv", a_data)
     if qa == "a":
         data = persistence.import_data_from_file("sample_data/answer.csv")
         id_index_number = None
@@ -30,3 +30,20 @@ def delete_by_id(qa, id):
                 id_index_number = counter
         del data[id_index_number]
         persistence.export_data_to_file("sample_data/answer.csv", data)
+
+def create_new_id(qa, _question_id= None):
+    _id=[]
+    if qa == "q":
+        data = persistence.import_data_from_file("sample_data/question.csv")
+        for dict in data:
+            _id.append(dict['id'])
+    if qa == 'a':
+        data = persistence.import_data_from_file("sample_data/answer.csv")
+        for dict in data:
+            if dict['question_id']== str(_question_id):
+                _id.append(dict['id'])
+    new_id= int(max(_id))
+    return new_id +1
+
+
+
