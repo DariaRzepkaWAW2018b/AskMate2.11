59ff4e25c81054e84043da49882f0602c3e0c222
Daria Rzepka <dariarz0110@gmail.com>
2018-05-30 10:56:29 +0200

    delete function in logic.py

--- logic.py
+++ logic.py
@@ -1,10 +1,23 @@
-#import persistance.py
+import persistence.py
 
 
-def delete (list, _id):
-#    import_data_from_file(list):
-    for dict in list:        
-        if dict['id'] == _id:
-            list.remove(dict)
-#            export_data_to_file(list):
-
+def delete_by_id(qa, id):
+    '''
+    Deletes post by id
+    Args:
+        qa - str q or a, q for question, a for answer
+        id - str or int - id of element to delete
+    '''
+    if qa == "q":
+        data = persistence.import_data_from_file("sample_data/question.csv")
+    if qa == "a":
+        data = persistence.import_data_from_file("sample_data/answer.csv")
+    id_index_number = None
+    for counter, entry in enumerate(data):
+        if str(entry['id']) == str(id):
+            id_index_number = counter
+    del data[id_index_number]
+    if qa == "q":
+        persistence.export_data_to_file("sample_data/question.csv", data)
+    if qa == "a":
+        persistence.export_data_to_file("sample_data/answer.csv", data)
