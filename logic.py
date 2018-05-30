c3ec867013c6e76c764c4f778a50dc34dfefe3f1
Daria Rzepka <dariarz0110@gmail.com>
2018-05-30 13:18:12 +0200

    delete with more power

--- logic.py
+++ logic.py
@@ -9,15 +9,24 @@ def delete_by_id(qa, id):
         id - str or int - id of element to delete
     '''
     if qa == "q":
-        data = persistence.import_data_from_file("sample_data/question.csv")
+        q_data = persistence.import_data_from_file("sample_data/question.csv")
+        a_data = persistence.import_data_from_file("sample_data/answer.csv")
+        id_index_number = None
+        for counter, entry in enumerate(q_data):
+            if str(entry['id']) == str(id):
+                id_index_number = counter
+        del q_data[id_index_number]
+        for counter, entry in enumerate(a_data):
+            if str(entry['question_id']) == str(id):
+                id_index_number = counter
+        del a_data[id_index_number]
+            persistence.export_data_to_file("sample_data/question.csv", q_data)
+            persistence.export_data_to_file("sample_data/answer.csv", a_data)
     if qa == "a":
         data = persistence.import_data_from_file("sample_data/answer.csv")
-    id_index_number = None
-    for counter, entry in enumerate(data):
-        if str(entry['id']) == str(id):
-            id_index_number = counter
-    del data[id_index_number]
-    if qa == "q":
-        persistence.export_data_to_file("sample_data/question.csv", data)
-    if qa == "a":
+        id_index_number = None
+        for counter, entry in enumerate(data):
+            if str(entry['id']) == str(id):
+                id_index_number = counter
+        del data[id_index_number]
         persistence.export_data_to_file("sample_data/answer.csv", data)
