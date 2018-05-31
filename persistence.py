1b2b4e3dda5e9d77f3b1a8f183d66991309df842
Svetlana.Syo <svetlana.syo@gmail.com>
2018-05-31 17:42:43 +0200

    Delete testes code line from file

--- persistence.py
+++ persistence.py
@@ -7,7 +7,7 @@ def import_headers_from_file(file_name):
         for line in csv_reader:
             return line
 
-
+print(import_headers_from_file('sample_data/question.csv'))
 
 
 def import_data_from_file(file_name):
@@ -18,6 +18,8 @@ def import_data_from_file(file_name):
         for row in csv_reader:
             list_of_dict.append(row)
         return list_of_dict
+
+print(import_data_from_file('sample_data/question.csv'))
 
 
 def export_data_to_file(path, data):
