ee48b94544cc30006485318224b2c3c6668fbd1f
Daniel Mielczarek <daniel1112m@gmail.com>
2018-06-13 23:48:43 +0200

    Add comment question, kilka drobnych zmian w templates

--- database_manager.py
+++ database_manager.py
@@ -3,14 +3,36 @@ from psycopg2 import sql
 
 
 
+@database_common.connection_handler
+def import_data_from_file_question(cursor):
+    cursor.execute("SELECT * FROM question")
+    # cursor.execute(
+    # sql.SQL("SELECT * FROM db_table VALUES (%s)"
+    #     .format(sql.Identifier("db_table"), db_table))
+    list_of_dict = cursor.fetchall()
+    return(list_of_dict)
+
+
 
 @database_common.connection_handler
-def import_data_from_file(cursor):
-    cursor.execute("SELECT * FROM question;")
+def import_data_from_file_answer(cursor):
+    cursor.execute("SELECT * FROM answer WHERE question_id = 1")
+    list_of_dict = cursor.fetchall()
+    return list_of_dict
+
+
+
+@database_common.connection_handler
+def import_data_from_file_comment(cursor):
+    cursor.execute("SELECT * FROM comment")
     list_of_dict = cursor.fetchall()
     return list_of_dict
 
 @database_common.connection_handler
 def add_data(cursor, new_title, new_message, new_image=None):
     cursor.execute("INSERT into question (title, message, image) VALUES (%s, %s, %s)", (new_title, new_message, new_image))
-    
+
+
+@database_common.connection_handler
+def add_comment(cursor, new_message):
+    cursor.execute("INSERT into comment (message) VALUES (%s)",(new_message,))
